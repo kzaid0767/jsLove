cb849c81dac83a28b78e429e10a9ed3a4f442e4d
Kassim Zaid <kzaid0767@gmail.com>
2022-06-29 10:32:47 -0400

    message good for deployment

--- secret_message_app/index.js
+++ secret_message_app/index.js
@@ -1,1 +1,26 @@
-console.log('hi there')
+/* selecting form and adding submit event listener and preventing default submission */
+const { hash } = window.location
+const message = atob(hash.replace('#', ''))
+
+if (message) {
+    document.querySelector('#message-form').classList.add('hide')
+    document.querySelector('#message-show').classList.remove('hide')
+    document.querySelector('h1').innerHTML = message
+
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault()
+
+    document.querySelector('#message-form').classList.add('hide')
+    document.querySelector('#message-link').classList.remove('hide')
+
+    const input = document.querySelector('#message-input')
+    const encrypted = btoa(input.value)
+
+    const encryptedInput = document.querySelector('#link-input')
+    encryptedInput.value = `${window.location}#${encrypted}`
+    encryptedInput.select()
+})
+
+/*  btoa() a js function that converts a strin to base 64  and atob() does the reverse*/
