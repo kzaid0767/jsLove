811e00d3d6c7236a81385015139715c0bd38489c
Kassim Zaid <kzaid0767@gmail.com>
2022-07-06 14:25:38 -0400

    up to lecture 417

--- e-commerce-app/routes/admin/auth.js
+++ e-commerce-app/routes/admin/auth.js
@@ -1,6 +1,6 @@
 const expres = require('express')
-const { check, validationResult } = require('express-validator')
 
+const {handleErrors} = require('./middlewares')
 const usersRepo = require('../../repositories/users')
 const signupTemplate = require('../../views/admin/auth/signup')
 const signinTemplate = require('../../views/admin/auth/signin')
@@ -16,20 +16,15 @@ router.get('/signup', (req, res) => {
 /* bodyParser middleware takes post info, parse it and make it req.body */
 router.post('/signup',
     [requireEmail, requirePassword, requirePasswordConfirmation],
+    handleErrors(signupTemplate),
     async (req, res) => {
-        const errors = validationResult(req)
-
-        if (!errors.isEmpty()) {
-            return res.send(signupTemplate({ req, errors }))
-        }
-
-        const { email, password, passwordConfirmation } = req.body
+        const { email, password } = req.body
 
         const user = await usersRepo.create({ email, password })
 
         req.session.userId = user.id
 
-        res.send('Account created')
+        res.redirect('/admin/products')
     })
 
 router.get('/signout', (req, res) => {
@@ -43,16 +38,13 @@ router.get('/signin', (req, res) => {
 
 router.post('/signin',
     [requireEmailExists, requireValidPasswordForUser],
+    handleErrors(signinTemplate),
     async (req, res) => {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()){
-            return res.send(signinTemplate({errors}))
-        }
         const { email } = req.body
         const user = await usersRepo.getOneBy({ email })
 
         req.session.userId = user.id
-        res.send('You are signed in!!')
+        res.redirect('/admin/products')
     })
 
 module.exports = router
